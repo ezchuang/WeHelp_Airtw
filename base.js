4b7d8e3288851dd85ebbdfee0bdba3c9fdb62a2e
wun-yu-lin <76545619+wun-yu-lin@users.noreply.github.com>
2023-10-18 02:49:35 +0800

    新增base.js 新增自動select選單在index.html

---
+++ base.js
@@ -0,0 +1,91 @@
+indexMain()
+var siteResultGlobal; //cite data global variable
+
+async function indexMain(){
+    let responeData = await classifySite()
+    siteResultGlobal = responeData
+    insertMonitoringStation("台中市")
+    
+    
+}
+
+
+
+// 生成觀測站資料
+function insertMonitoringStation(stationName){
+    let monitoringStationSelect = document.querySelector(".realtimeAirMonitor_content_right_select_left_select")
+    monitoringStationSelect.innerHTML = ""
+    let option = document.createElement("option")
+    option.value = ""
+    option.innerText = "請選擇監測站"
+    monitoringStationSelect.appendChild(option)
+    let stationArr = siteResultGlobal[stationName]
+
+  
+    stationArr.forEach(element=>{
+        let option = document.createElement("option")
+        option.value = element
+        option.innerText = element
+        monitoringStationSelect.appendChild(option)
+    })
+}
+
+
+
+
+// 站點資料請呼叫 async function classifySite()
+// 請注意 因需要 fetch API，所以 classifySite() return 的結果為非同步資料
+// 請於對應位置加上 call back or await 來處理
+
+
+function getParasSite(){
+    let queryCluster = {
+        "originUrl" : "https://data.moenv.gov.tw/api/v2/aqx_p_07",
+        "language" : null,
+        "offset" : null,
+        "limit" : null,
+        "apiKey" : "e8dd42e6-9b8b-43f8-991e-b3dee723a52d",
+    }
+
+    return queryCluster
+}
+
+
+function combineUrlSite(queryCluster){
+    let url = `${queryCluster.originUrl}?api_key=${queryCluster.apiKey}`
+
+    if (queryCluster.offset){
+        url += `&offset=${queryCluster.offset}`
+    }
+    if (queryCluster.limit){
+        url += `&limit=${queryCluster.limit}`
+    }
+
+    return url
+}
+
+
+async function getAPISite(){
+    let queryCluster = getParasSite()
+    let url = combineUrlSite(queryCluster)
+
+    let data = await fetch(url)
+    data = await data.json()
+
+    return data
+}
+
+
+async function classifySite(){
+    let siteDataRaw = await getAPISite()
+    let siteData = {}
+    siteDataRaw.records.forEach(element => {
+        if (! Object.keys(siteData).includes(element.county)){
+            siteData[element.county] = [element.sitename]
+        }else{
+            siteData[element.county].push(element.sitename)
+        }
+    })
+
+    return siteData
+}
